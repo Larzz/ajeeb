75bb630398996423224ef7acdc13961393d24bc0
Larry <larry@creativouae.com>
2020-10-19 07:52:23 +0400

    hello

--- resources/views/product_page.blade.php
+++ resources/views/product_page.blade.php
@@ -23,85 +23,176 @@
         <div class="container">
             <div class="row">
                 <div class="reservation-items about-items">
+                    <div class="col-md-5 form">
+                        <div class="form-box" style="padding: 0px 0px 0px 0px;">
+                            <div class="avatar">
+                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="Thumb">
+                             
+                            </div>
+                            <div class="sidebar-item gallery">
+                                <div class="sidebar-info">
+                                    <ul>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <img src="http://ajeeb.test/images/products/white_meat_tuna_olive_oil.png" alt="thumb">
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                         
+                        </div>
+                    </div>
                     <div class="col-md-7 info">
                         <h2>How do i make raservations</h2>
                         <p>
                             Pleased anxious or as in by viewing forbade minutes prevent. Too leave had those get being led weeks blind. Had men rose from down lady able. Its son him ferrars proceed six parlors.
                         </p>
-                        <p>
-                           Advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket.
-                        </p>
-                        <div class="address">
-                            <ul>
-                                <li>
-                                    <span>Address</span>
-                                    <p>
-                                        22 Baker Street, London, United Kingdom, W1U 3BW
-                                    </p>
-                                </li>
-                                <li>
-                                    <span>Phone</span>
-                                    <p>
-                                        +123 456 7890
-                                    </p>
-                                </li>
-                                <li>
-                                    <span>Email</span>
-                                    <p>
-                                        support@restcafe.com
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                     <!-- Start Form -->
-                    <div class="col-md-5 form">
-                        <div class="form-box">
-                            <div class="icon">
-                                <i class="fas fa-utensils"></i>
-                            </div>
-                            <h3>Book a table</h3>
-                            <form action="#">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input class="form-control" id="name" name="name" placeholder="Name" type="text">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input class="form-control" id="email" name="email" placeholder="Email" type="email">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <select style="display: none;">
-                                                <option value="1">1 Person</option>
-                                                <option value="2">2 Person</option>
-                                                <option value="4">3 Person</option>
-                                                <option value="5">4 Person</option>
-                                            </select><div class="nice-select" tabindex="0"><span class="current">3 Person</span><ul class="list"><li data-value="1" class="option">1 Person</li><li data-value="2" class="option focus">2 Person</li><li data-value="4" class="option selected">3 Person</li><li data-value="5" class="option">4 Person</li></ul></div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input class="form-control" id="date" name="date" placeholder="Date" type="text">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <button type="submit" name="submit" id="submit">
-                                            Book Now
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- End Form -->
+                    
+
+                 
+                    
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="food-menu-area path-less carousel-shadow default-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="site-heading text-center">
+                        <h3>Discorver</h3>
+                        <h2>Our specials</h2>
+                        <p>
+                            While mirth large of on front. Ye he greater related adapted proceed entered an. Through it examine express promise no. Past add size game cold girl off how old
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="menu-lists food-menu-carousel owl-carousel owl-theme text-center">
+                        <!-- Single Item -->
+                        <div class="item">
+                            <div class="thumb">
+                                <a href="#">
+                                    <img src="assets/img/800x600.png" alt="Thumb">
+                                </a>
+                                <div class="price">
+                                    <h5>$5.90</h5>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <h4><a href="#">Crispy Crust Pizzeria</a></h4>
+                                <span>Mutton / Olive Oil / Salt</span>
+                                <p>
+                                    Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.
+                                </p>
+                                <div class="button">
+                                    <a href="#">Order Now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="item">
+                            <div class="thumb">
+                                <a href="#">
+                                    <img src="assets/img/800x600.png" alt="Thumb">
+                                </a>
+                                <div class="price">
+                                    <h5>$18.10</h5>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <h4><a href="#">Luger Burger</a></h4>
+                                <span>Beef / Olive Oil / Salt</span>
+                                <p>
+                                    Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.
+                                </p>
+                                <div class="button">
+                                    <a href="#">Order Now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="item">
+                            <div class="thumb">
+                                <a href="#">
+                                    <img src="assets/img/800x600.png" alt="Thumb">
+                                </a>
+                                <div class="price">
+                                    <h5>$6.00</h5>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <h4><a href="#">Fries McDonald's</a></h4>
+                                <span>Chicken / Olive Oil / Salt</span>
+                                <p>
+                                    Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.
+                                </p>
+                                <div class="button">
+                                    <a href="#">Order Now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="item">
+                            <div class="thumb">
+                                <a href="#">
+                                    <img src="assets/img/800x600.png" alt="Thumb">
+                                </a>
+                                <div class="price">
+                                    <h5>$11.50</h5>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <h4><a href="#">Chicken Popeyes</a></h4>
+                                <span>Mutton / Olive Oil / Salt</span>
+                                <p>
+                                    Considered introduced themselves mr to discretion at. Means among saw hopes for. Death mirth in oh learn he equal on.
+                                </p>
+                                <div class="button">
+                                    <a href="#">Order Now</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>git 
 
 @endsection
 
